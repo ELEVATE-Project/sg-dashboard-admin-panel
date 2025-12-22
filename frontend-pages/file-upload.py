fd5c192505a547c48515f8723c63e14b21b70e78
GSuma21 <21gsuma@gmail.com>
2025-12-22 10:00:26 +0530

    optimised line chart with voice tab line chart

--- frontend-pages/file-upload.py
+++ frontend-pages/file-upload.py
@@ -6,7 +6,7 @@ import re
 from tabs_scripts.community_led_details import community_led_programs_sum_with_codes, pie_chart_community_led
 from tabs_scripts.goals import goals
 from tabs_scripts.key_progress_indicators import key_progress_indicators
-from tabs_scripts.line_chart import extract_district_line_chart, extract_micro_improvements, extract_state_line_chart
+from tabs_scripts.line_chart import extract_district_line_chart, extract_micro_improvements, extract_state_line_chart, update_voices_json
 from tabs_scripts.network_map_data import get_network_map_data
 from tabs_scripts.partners import get_partners
 from tabs_scripts.extract_state_details import update_district_view_indicators
@@ -16,7 +16,7 @@ from tabs_scripts.programs import generate_program_reports
 from tabs_scripts.extract_district_details import extract_district_details
 from tabs_scripts.extract_community_details import extract_community_details
 from tabs_scripts.upload_images_from_excel import upload_images_from_excel
-from tabs_scripts.voices_tab_big_numbers import voices_tab_big_numbers, update_voices_json
+from tabs_scripts.voices_tab_big_numbers import voices_tab_big_numbers
 
 from constants import ALLOWED_TABS as allowed_tabs
 
@@ -198,7 +198,7 @@ if uploaded_file is not None:
                     extract_micro_improvements(uploaded_file)
                     upload_images_from_excel(uploaded_file)
                     voices_tab_big_numbers(uploaded_file)
-                    update_voices_json(uploaded_file)
+                    update_voices_json_line_chart(uploaded_file)
                     status.update(label="✅ All files uploaded successfully!", state="complete")
             except Exception as e:
                 st.error(f"❌ Error during full upload: {e}")
